a8c23f5be573a08bb55b1396ce2a915cf402aa74
Carl <qwerfcxzas1@gmail.com>
2023-02-28 12:02:15 +0800

    全局引入elementUI

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <h1>Hello</h1>
+    <el-button>按我</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+  }
+}
+</script>
+<style scoped>
+
+</style>
