d61624a23d89e42f3674dce1e78fc956598026ce
Carl <qwerfcxzas1@gmail.com>
2023-02-28 12:11:31 +0800

    element按需使用

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
 
-Vue.use(ElementUI)
+import './plugins/element'
+
+
+
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
+
+
+
+
+// import ElementUI from "element-ui";
+// import "element-ui/lib/theme-chalk/index.css";
+// Vue.use(ElementUI)
